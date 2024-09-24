37b36ebbd3734dd93e5b02db771c8e51907c99cd
Kim Petersson <kim.petersson301@gmail.com>
2024-09-24 11:53:20 +0200

    test commit

--- BurgerOrderer/burgerorderer.py
+++ BurgerOrderer/burgerorderer.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 """ BurgerOrderer"""
-=======
->>>>>>> 798b60dfadfa0782d10400ae82c1c88814de78de
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 @app.route("/")
 def front_page():
     """ Frontpage """
@@ -14,11 +10,3 @@ def front_page():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
-=======
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> 798b60dfadfa0782d10400ae82c1c88814de78de
