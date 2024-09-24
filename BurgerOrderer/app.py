6241652bb4b5ae3f68fb3df8ce8ae8da23ef5885
Kim Petersson <kim.petersson301@gmail.com>
2024-09-24 13:34:42 +0200

    added TODO comments

--- BurgerOrderer/app.py
+++ BurgerOrderer/app.py
@@ -8,5 +8,10 @@ def front_page():
     """ Frontpage """
     return render_template("index.html")
 
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
+
+#TODO: Lägg till fler app.routes för de andra sidorna.
+#TODO: Bygg ut front-pagen med länk till sidan för att välja meny etc
