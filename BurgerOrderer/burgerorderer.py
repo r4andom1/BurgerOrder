7f25ec29f418bbe32bedc53854083b523bc6910e
Kim Petersson <kim.petersson301@gmail.com>
2024-09-24 11:38:44 +0200

    added more code for burgerorderer.py

---
+++ BurgerOrderer/burgerorderer.py
@@ -0,0 +1,12 @@
+""" BurgerOrderer"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def front_page():
+    """ Frontpage """
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
