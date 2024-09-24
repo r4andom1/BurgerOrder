4c28717550e560ba6a332fa9575fc09d5586641e
Jesper <Jesperrudegran@gmail.com>
2024-09-24 13:38:31 +0200

    Added templates subfolder and home.html

--- BurgerOrderer/app.py
+++ BurgerOrderer/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def front_page():
     """ Frontpage """
-    return render_template("index.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
