de84968c1d78c7dc9a15e93d0397f66c378b94ee
Kim Petersson <kim.petersson301@gmail.com>
2024-10-08 15:13:01 +0200

    added structure for kitchenview app.py and burgerorderer app.py to make connection work, still have work to do here

--- BurgerOrderer/app.py
+++ BurgerOrderer/app.py
@@ -1,18 +1,19 @@
 """ BurgerOrderer"""
 from flask import Flask, render_template
+from flask import request
 from db import connect
+import requests
 import db_fetch
-
-
-
-db = SQLAlchemy() # db intitialized here
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://test.db"
-db.init_app(app)
-
-
+import os
 
 app = Flask(__name__)
+
+order_dict = {
+    "name": "big_burger",
+    "quantity": 2,
+    "price": 25,
+    "modifications": []
+}
 
 @app.route("/toppings")
 def toppings():
@@ -20,7 +21,6 @@ def toppings():
 
     toppings_html = db_fetch.fetch_topping_html()
     return render_template("toppings.html", toppings_list=toppings_html)
-
 
 @app.route("/")
 def front_page():
@@ -32,14 +32,12 @@ def order_page():
     """ Order page """
     return render_template("order-page.html")
 
-
 @app.route("/order/<category>", methods=['get'])
 def order_category(category):
     """ Menu choice page """
 
     items = db_fetch.fetch_category(category)
     return render_template("order-category.html", category=category, items=items)
-
 
 @app.route("/order-completed")
 def order_completed_page():
@@ -58,8 +56,46 @@ def menu_info(menu_id):
     menu = db_fetch.fetch_menu(menu_id)
     return render_template("menu-info.html", menu=menu)
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+""" Everything under here is about the kitchenview connection and sending the order through """
+base_url = 'http://' + os.getenv('KITCHENVIEW_HOST', 'localhost:5000')
 
-#TODO: Lägg till fler app.routes för de andra sidorna.
-#TODO: Bygg ut front-pagen med länk till sidan för att välja meny etc
+def render_order_page(burger_name, args):
+    return "ordered " + burger_name
+
+def make_url(burger_name):
+    """ builds url to kitchenview with burger name"""
+    return base_url + "/buy" + burger_name
+
+def add_options(url, args):
+    """
+    adds the options the user has added or removed from order to the url
+    """
+    if len(args) != 0:
+        url += "?" # adds a ? to divide the link from the args
+        for arg in args:
+            url += arg + "&" # adds a "&" between every option added-on
+        return url
+    
+def send_to_kitchen(burger_name, args):
+    """ 
+    Creates url and adds the options,
+    then sends url to kitchenview.
+    """
+    requrl = make_url(burger_name)
+    requrl = add_options(requrl, args)
+
+    print("Using kitchenview url: " + requrl)
+    requests.get(requrl)
+    return
+
+@app.route("/buy/<burgername>", methods=["get"])
+def buy(burger_name):
+    """ should """
+    print("Placing an order on " + burger_name)
+    send_to_kitchen(burger_name, request.args)
+    return render_order_page(burger_name, request.args)
+
+if __name__ == "__main__":
+    #print(base_url)
+    app.run(debug=True, host="0.0.0.0", port=8000)
+    
--- KitchenView/app.py
+++ KitchenView/app.py
@@ -1,6 +1,18 @@
-from flask import Flask, request
+from flask import Flask
+from flask import request
 
 app = Flask(__name__)
+
+@app.route("/")
+def frontpage():
+    return 'Please specify a burger to buy'
+
+@app.route("/buy/<burgerName>", methods=["get"])
+def buy(burger_name):
+    print("One " + burger_name + " ordered with the following options:")
+    for arg in request.args:
+        print(" - " + arg)
+    return "Ok"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
