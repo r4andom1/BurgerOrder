6c2fc0098c5c5aa612673b9c1e224c7878e80732
André Rosenquist <rosenquist96@gmail.com>
2024-10-08 14:37:16 +0200

    Added some more test cod so we are checking all the containers in the data base

--- tests/test.py
+++ tests/test.py
@@ -14,7 +14,9 @@ def connect():
     conn.set_client_encoding('UTF8')
     return conn
 
-def database_test():
+# _____________________________________________________
+
+def database_test_products():
     conn = connect()
     if conn is not None:
         try:
@@ -30,8 +32,92 @@ def database_test():
     else:
         print("Connection to the database failed.")
 
+# _____________________________________________________
+
+def database_test_menus():
+    conn = connect()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM menus")
+            toppings = cursor.fetchone()
+            print(toppings)
+        except Exception as e:
+            print(f"Error executing query: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+    else:
+        print("Connection to the database failed.")
+
+# _____________________________________________________
+
+def database_test_toppings():
+    conn = connect()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM toppings")
+            toppings = cursor.fetchone()
+            print(toppings)
+        except Exception as e:
+            print(f"Error executing query: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+    else:
+        print("Connection to the database failed.")
+
+# _____________________________________________________
+
+def database_test_topping_type():
+    conn = connect()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM ref_topping_type")
+            toppings = cursor.fetchall()
+            print(toppings)
+        except Exception as e:
+            print(f"Error executing query: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+    else:
+        print("Connection to the database failed.")
+
+# _____________________________________________________
+
+def database_test_product_type():
+    conn = connect()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM ref_product_type")
+            toppings = cursor.fetchall()
+            print(toppings)
+        except Exception as e:
+            print(f"Error executing query: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+    else:
+        print("Connection to the database failed.")
+
+
 products_table_test = (1, 1, "Plantera Burger", "A perfect looking gluten free burger.", "Decimal('7.99')", 1, 1)
+menus_table_test = (1, 'Big Juicy Burger Combo', 'Its big... Its juicy...', "Decimal('4.99')")
+toppings_table_test = (1, 'Brioche', 'Soft and buttery French-style bread.', "Decimal('1.50')", 1)
+topping_type_table_test = [(1, 'Bread'), (2, 'Meat'), (3, 'Topping'), (4, 'Sauce')]
+product_type_table_test = [(1, 'burger'), (2, 'drink'), (3, 'side')]
+
 
 
 if __name__ == "__main__":
-    database_test()
+
+
+    database_test_products()
+    database_test_menus()
+    database_test_toppings()
+    database_test_topping_type()
+    database_test_product_type()
