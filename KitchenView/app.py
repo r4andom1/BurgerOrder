7466c5d134cafe0345f060a3608fe3fda8c7e53d
Kim Petersson <kim.petersson301@gmail.com>
2024-10-10 15:16:10 +0200

    formated order print a little

--- KitchenView/app.py
+++ KitchenView/app.py
@@ -16,24 +16,19 @@ def buy(burger_name):
 
 @app.route("/order", methods=["POST"])
 def order():
-    print("Order placed")
     shopping_cart = json.loads(request.json)
     print_order(shopping_cart)
     return "200"
 
 def print_order(shopping_cart):
     cart_addons = shopping_cart["products"][0]["modifications"]
-    if shopping_cart["products"][0]["quantity"] != 1: # If quantity is not 1
-        print("Print order as usual\n")
-        print(shopping_cart["products"][0]["quantity"])
-    #else
     for item in shopping_cart["products"]:
-        print(item["name"])
+        print("\n" + item["name"])
         if item["modifications"]:
+            print(f"Customer has modified order with: \n")
             for addon in item["modifications"]:
                 if addon["quantity"] != 1:
-                    print(addon["topping_name"])
-                    print(addon["quantity"])
+                    print(f"{addon['quantity']} {addon['topping_name']}")
 
 
 if __name__ == "__main__":
