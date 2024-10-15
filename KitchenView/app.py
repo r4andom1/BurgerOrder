cd1ed4519b6f3dd7b5a88383c84a09edbf173bc7
Kim Petersson <kim.petersson301@gmail.com>
2024-10-15 10:58:12 +0200

    cleaned up some code in app.py, removed unnessecary logic

--- KitchenView/app.py
+++ KitchenView/app.py
@@ -6,20 +6,6 @@ from flask import Flask, request, json
 
 app = Flask(__name__)
 
-""" Example code for building order 
-# @app.route("/")
-# def frontpage():
-#     return 'Please specify a burger to buy'
-
-# @app.route("/buy/<burger_name>")
-# def buy(burger_name):
-#     print("Order placed")
-#     print("One " + burger_name + " ordered with the following options:")
-#     for arg in request.args:
-#         print(" - " + arg)
-#     return "Ok"
-"""
-
 @app.route("/order", methods=["POST"])
 def order():
     """
@@ -35,7 +21,6 @@ def print_order(shopping_cart):
     Prints formatted order and any 
     modifications if there is any. 
     """
-    cart_addons = shopping_cart["products"][0]["modifications"]
     print("Order has been placed:")
     print("-----------------") # formatting
     for item in shopping_cart["products"]:
